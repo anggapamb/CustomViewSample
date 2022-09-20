1d047ee6f90c139cef4bd5b93a5669269e91657c
anggapamb <email_anda>
2022-09-20 10:40:30 +0700

    first commit

---
+++ ticketingapps/src/main/java/com/anggapamb/ticketingapps/SeatsView.kt
@@ -0,0 +1,52 @@
+package com.anggapamb.ticketingapps
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import com.anggapamb.ticketingapps.data.Seat
+
+class SeatsView: View {
+
+    private val seats: ArrayList<Seat> = arrayListOf(
+        Seat(id = 1, name = "A1", isBooked = false),
+        Seat(id = 2, name = "A2", isBooked = false),
+        Seat(id = 3, name = "B1", isBooked = false),
+        Seat(id = 4, name = "A4", isBooked = false),
+        Seat(id = 5, name = "C1", isBooked = false),
+        Seat(id = 6, name = "C2", isBooked = false),
+        Seat(id = 7, name = "D1", isBooked = false),
+        Seat(id = 8, name = "D2", isBooked = false),
+    )
+
+    constructor(context: Context): super(context)
+
+    constructor(context: Context, attrs: AttributeSet): super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int): super(context, attrs, defStyleAttr)
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val width = getDefaultSize(suggestedMinimumWidth, widthMeasureSpec)
+        val height = getDefaultSize(suggestedMinimumHeight, heightMeasureSpec)
+
+        val halfOfHeight = height / 2
+        val halfOfWidth = width / 2
+        var value = -600F
+
+        for (i in 0..7) {
+            if (i.mod(2) == 0) {
+                seats[i].apply {
+                    x = halfOfWidth - 300F
+                    y = halfOfHeight + value
+                }
+            } else {
+                seats[i].apply {
+                    x = halfOfWidth + 100F
+                    y = halfOfHeight + value
+                }
+                value += 300F
+            }
+        }
+    }
+
+}
---
+++ ticketingapps/src/main/java/com/anggapamb/ticketingapps/data/Seat.kt
@@ -0,0 +1,10 @@
+package com.anggapamb.ticketingapps.data
+
+data class Seat(
+    val id: Int,
+    var x: Float? = 0F,
+    var y: Float? = 0F,
+    var name: String,
+    var isBooked: Boolean
+)
+
